b833926ca583f49f379a251cf79de55d87edc38d
sangeeta <sangeetasonal123@gmail.com>
2024-06-22 00:19:31 +0530

    connect

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
   
 const App = () => {
 
-const url = "http://localhost:4000"
+const url = "https://food-del-web-3.onrender.com"
 
   return (
     <div>
