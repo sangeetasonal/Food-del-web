663f42653ef080165ea156710b04893c2a2ecb5c
sangeeta <sangeetasonal123@gmail.com>
2024-06-22 00:27:23 +0530

    link

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Placing user order for frontend
 const placeOrder = async (req, res) => {
-    const frontend_url = "http://localhost:5176";
+    const frontend_url = "https://silver-treacle-22f36e.netlify.app/";
 
     try {
         const newOrder = new orderModel({
